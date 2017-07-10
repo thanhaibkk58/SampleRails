99398d48dfb5e5a70a5b435f0b5dd25499f84710
thanhaibkk58 <thanhaibkk58@gmail.com>
2017-07-10 09:54:54 +0700

    user-microposts-1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  private
+
+  def logged_in_user
+    return true if logged_in?
+    store_location
+    flash[:danger] = t ".loggin_require"
+    redirect_to login_url
+  end
 end
